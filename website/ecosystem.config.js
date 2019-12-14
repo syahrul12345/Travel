8fc37cb32c29a5bb86432c42e37879fd9eb093c4
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2019-12-14 19:53:23 +0800

    Basic staging

--- website/ecosystem.config.js
+++ website/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+    name: 'website',
+    script: 'npm run next',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
